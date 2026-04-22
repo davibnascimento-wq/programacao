d9467e5a206dc153e8fdf2b9298d4e8c20632be3
davibnascimento-wq <davi_b_nascimento@estudante.sesisenai.org.br>
2026-04-22 14:18:52 +0000

    update

---
+++ lista 1  python/atividade27.py
@@ -0,0 +1,5 @@
+numero1 = float(input("numero 1:"))
+numero2 = float(input("numero 2:"))
+numero3 = float(input("numero 3:"))
+total = numero1/numero2/numero3
+print (f"o valor da divisao e: ", total)
